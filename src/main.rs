a4f97e57e70da80b56bff16d67e4bc60a0130c81
Yorick van der Vis <yorick@fastmail.com>
2023-04-10 20:45:30 +0200

    Image generation + output redirect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::char::MAX;
+
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    // Render
+
+    println!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev(){
+        for i in 0..IMAGE_WIDTH {
+            let r: f32 = i as f32 / IMAGE_WIDTH as f32;
+            let g: f32 = j as f32 / IMAGE_HEIGHT as f32;
+            let b: f32 = 0.2;
+
+            let ir: i32 = (255.99 * r) as i32;
+            let ig: i32 = (255.99 * g) as i32;
+            let ib: i32 = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib)
+        }
+    }
 }
